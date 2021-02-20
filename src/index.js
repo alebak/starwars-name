e714ecbc2675e595b3723936866b2c4c5b1f44aa
Alebak <alebak@ximware.com>
2021-02-20 10:40:11 -0500

    feat(dashboard): add new features (feat 1, feat 2 and feat 3)

--- src/index.js
+++ src/index.js
@@ -13,3 +13,8 @@ console.log("Other message");
 
 console.log("This is the dashboard feature initial message");
 console.log("Fix 1 dashboard");
+
+// Pack dashboar feat
+console.log("Feat 1 dashboard");
+console.log("Feat 2 dashboard");
+console.log("Feat 3 dashboard");
